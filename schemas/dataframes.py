5cf5fc3a2da6c752fba8c365cfda7965df4198b8
rKorniets <rostyslav113@gmail.com>
2023-11-25 19:16:55 +0200

    Update dataframes.py

--- schemas/dataframes.py
+++ schemas/dataframes.py
@@ -1,5 +1,6 @@
 import sys
 import pathlib
+import pyspark.sql.types as t
 from schemas.imdb_schema import akas_schema, basics_schema, crew_schema, episode_schema, principals_schema, ratings_schema, name_schema
 
 def get_project_dir():
@@ -13,13 +14,21 @@ print(project_dir)
 
 # create dataframes for schemas
 def get_akas_df(spark):
-    return spark.read.csv(str(project_dir) + '/data/title.akas.tsv', sep=r'\t', schema=akas_schema, header=True)
+    df = spark.read.csv(str(project_dir) + '/data/title.akas.tsv', sep=r'\t', schema=akas_schema, header=True)
+    df = df.withColumn('types', t.ArrayType(t.StringType(), True))
+    df = df.withColumn('attributes', t.ArrayType(t.StringType(), True))
+    return df
 
 def get_basics_df(spark):
-    return spark.read.csv(str(project_dir) + '/data/title.basics.tsv', sep=r'\t', schema=basics_schema, header=True)
+    df = spark.read.csv(str(project_dir) + '/data/title.basics.tsv', sep=r'\t', schema=basics_schema, header=True)
+    df = df.withColumn('genres', t.ArrayType(t.StringType(), True))
+    return df
 
 def get_crew_df(spark):
-    return spark.read.csv(str(project_dir) + '/data/title.crew.tsv', sep=r'\t', schema=crew_schema, header=True)
+    df = spark.read.csv(str(project_dir) + '/data/title.crew.tsv', sep=r'\t', schema=crew_schema, header=True)
+    df = df.withColumn('directors', t.ArrayType(t.StringType(), True))
+    df = df.withColumn('writers', t.ArrayType(t.StringType(), True))
+    return df
 
 def get_episode_df(spark):
     return spark.read.csv(str(project_dir) + '/data/title.episode.tsv', sep=r'\t', schema=episode_schema, header=True)
@@ -31,4 +40,7 @@ def get_ratings_df(spark):
     return spark.read.csv(str(project_dir) + '/data/title.ratings.tsv', sep=r'\t', schema=ratings_schema, header=True)
 
 def get_name_df(spark):
-    return spark.read.csv(str(project_dir) + '/data/name.basics.tsv', sep=r'\t', schema=name_schema, header=True)
+    df = spark.read.csv(str(project_dir) + '/data/name.basics.tsv', sep=r'\t', schema=name_schema, header=True)
+    df = df.withColumn('knownForTitles', t.ArrayType(t.StringType(), True))
+    df = df.withColumn('primaryProfession', t.ArrayType(t.StringType(), True))
+    return df
