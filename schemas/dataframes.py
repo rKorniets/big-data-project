32af93053d23574b0311668304980bcc54991ea2
rKorniets <rostyslav113@gmail.com>
2023-11-25 19:31:21 +0200

    type fix 2

--- schemas/dataframes.py
+++ schemas/dataframes.py
@@ -1,6 +1,7 @@
 import sys
 import pathlib
 import pyspark.sql.types as t
+import pyspark.sql.functions as f
 from schemas.imdb_schema import akas_schema, basics_schema, crew_schema, episode_schema, principals_schema, ratings_schema, name_schema
 
 def get_project_dir():
@@ -15,19 +16,19 @@ print(project_dir)
 # create dataframes for schemas
 def get_akas_df(spark):
     df = spark.read.csv(str(project_dir) + '/data/title.akas.tsv', sep=r'\t', schema=akas_schema, header=True)
-    df = df.withColumn('types', t.ArrayType(t.StringType(), True))
-    df = df.withColumn('attributes', t.ArrayType(t.StringType(), True))
+    df = df.withColumn('types', f.split(df.types, ','))
+    df = df.withColumn('attributes', f.split(df.attributes, ','))
     return df
 
 def get_basics_df(spark):
     df = spark.read.csv(str(project_dir) + '/data/title.basics.tsv', sep=r'\t', schema=basics_schema, header=True)
-    df = df.withColumn('genres', t.ArrayType(t.StringType(), True))
+    df = df.withColumn('genres', f.split(df.genres, ','))
     return df
 
 def get_crew_df(spark):
     df = spark.read.csv(str(project_dir) + '/data/title.crew.tsv', sep=r'\t', schema=crew_schema, header=True)
-    df = df.withColumn('directors', t.ArrayType(t.StringType(), True))
-    df = df.withColumn('writers', t.ArrayType(t.StringType(), True))
+    df = df.withColumn('directors', f.split(df.directors, ','))
+    df = df.withColumn('writers', f.split(df.writers, ','))
     return df
 
 def get_episode_df(spark):
@@ -41,6 +42,6 @@ def get_ratings_df(spark):
 
 def get_name_df(spark):
     df = spark.read.csv(str(project_dir) + '/data/name.basics.tsv', sep=r'\t', schema=name_schema, header=True)
-    df = df.withColumn('knownForTitles', t.ArrayType(t.StringType(), True))
-    df = df.withColumn('primaryProfession', t.ArrayType(t.StringType(), True))
+    df = df.withColumn('knownForTitles', f.split(df.knownForTitles, ','))
+    df = df.withColumn('primaryProfession', f.split(df.primaryProfession, ','))
     return df
